df00cd3bf2f908a199bfd4a27172b4ecca49d3c6
priya-taee <157573598+priya-taee@users.noreply.github.com>
2024-01-24 13:12:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
+streamlit.title('My Parents Healthy Diner')
 streamlit.title('Breakfast Menu')
-
+streamlit.title('Omega 3 and Blueberry Oatmeal')
+streamlit.title('Kale, Spinach and Rocket smoothie')
+streamlit.title('Hard-Boiled Free Range Ege')
